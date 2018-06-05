738c54301d5e9450f1d97bb81f9a41694f09420f
wdqcode <dwangwd@gmail.com>
2018-06-05 10:06:37 +0800

    cmake/Dependencies.cmake: opencv_videoio library required

--- cmake/Dependencies.cmake
+++ cmake/Dependencies.cmake
@@ -62,6 +62,7 @@ endif()
 list(APPEND Caffe_INCLUDE_DIRS PUBLIC ${HDF5_INCLUDE_DIRS})
 list(APPEND Caffe_LINKER_LIBS PUBLIC ${HDF5_LIBRARIES} ${HDF5_HL_LIBRARIES})
 
+
 # ---[ LMDB
 if(USE_LMDB)
   find_package(LMDB REQUIRED)
@@ -109,9 +110,9 @@ endif()
 
 # ---[ OpenCV
 if(USE_OPENCV)
-  find_package(OpenCV QUIET COMPONENTS core highgui imgproc imgcodecs)
+  find_package(OpenCV QUIET COMPONENTS core highgui imgproc imgcodecs videoio)
   if(NOT OpenCV_FOUND) # if not OpenCV 3.x, then imgcodecs are not found
-    find_package(OpenCV REQUIRED COMPONENTS core highgui imgproc)
+    find_package(OpenCV REQUIRED COMPONENTS core highgui imgproc videoio)
   endif()
   list(APPEND Caffe_INCLUDE_DIRS PUBLIC ${OpenCV_INCLUDE_DIRS})
   list(APPEND Caffe_LINKER_LIBS PUBLIC ${OpenCV_LIBS})
