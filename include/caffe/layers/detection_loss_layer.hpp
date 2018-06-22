e4e8ca49ddb41dbf1541e4b662d83d0a4e5006da
wdker <dwangwd@gmail.com>
2018-06-22 14:25:29 +0800

    modified:   include/caffe/layers/detection_loss_layer.hpp

--- include/caffe/layers/detection_loss_layer.hpp
+++ include/caffe/layers/detection_loss_layer.hpp
@@ -33,13 +33,16 @@ class DetectionLossLayer : public LossLayer<Dtype> {
  protected:
   virtual void Forward_cpu(const vector<Blob<Dtype>*>& bottom,
       const vector<Blob<Dtype>*>& top);
-  // virtual void Forward_gpu(const vector<Blob<Dtype>*>& bottom,
-  //     const vector<Blob<Dtype>*>& top);
+//   virtual void Forward_gpu(const vector<Blob<Dtype>*>& bottom,
+//      const vector<Blob<Dtype>*>& top);
+
 
   virtual void Backward_cpu(const vector<Blob<Dtype>*>& top,
       const vector<bool>& propagate_down, const vector<Blob<Dtype>*>& bottom);
-  // virtual void Backward_gpu(const vector<Blob<Dtype>*>& top,
-  //     const vector<bool>& propagate_down, const vector<Blob<Dtype>*>& bottom);
+
+//  virtual void Backward_gpu(const vector<Blob<Dtype>*>& top,
+//     const vector<bool>& propagate_down, const vector<Blob<Dtype>*>& bottom);
+
 
   int side_;
   int num_class_;
